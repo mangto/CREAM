55faf8a923500d08c89381c3006f84b5fe819cd5
mangomangto <70062747+mangto@users.noreply.github.com>
2022-07-16 00:36:07 +0900

    Update cream-beta.py

--- cream-beta.py
+++ cream-beta.py
@@ -7,10 +7,12 @@ def RotateWeight(weights:numpy.array):
     return numpy.flip(numpy.rot90(weights,k=-1),1)
 
 class network:
-    def __init__(self, NetworkShape:list):
+    def __init__(self, LearningRate:float, NetworkShape:list):
         self.NetworkShape = NetworkShape
         self.weights = [numpy.array([list(numpy.random.randn(i)) for j in range(NetworkShape[k+1])]) for k, i in enumerate(NetworkShape[0:-1])]
         self.activations = self.reset_activation()
+
+        self.l_rate = LearningRate
 
     def reset_activation(self):
         self.activations = [ [0 for i in range(hn)] for hn in self.NetworkShape]
@@ -31,13 +33,21 @@ class network:
             self.activations[i+1] = active
 
 
-    def backward(self, RValue:list):
-        if (len(RValue) != self.NetworkShape[-1]): raise ValueError("Worng Counts of RValue (=RValue)")
+    def error(self, RValue:list):
+        if (len(RValue) != self.NetworkShape[-1]): raise ValueError("Worng Counts of 'RValue' (=RValue)")
 
-        errors = [self.activations[-1][i] - RValue[i] for i in range(self.NetworkShape[-1])]
+        errors = [(RValue[i]-self.activations[-1][i])**2/2 for i in range(self.NetworkShape[-1])]
+        return errors
 
-        print(errors)
+    def backpropgation(self, error):
+        if (len(error) != self.NetworkShape[-1]): raise ValueError("Worng Counts of 'error'")
 
-net = network([10,5,3,2])
+
+        for i, weight in enumerate(reversed(self.weights)):
+            print(weight)
+
+
+
+net = network(0.6,[10,5,3,2])
 net.forward([1,2,3,4,5,6,7,8,9,10])
-net.backward([1,0])
+print(net.error([1,0]))
