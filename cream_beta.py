01b782acac29fb47acfcae3e99d39c1b7667fbe4
mangomangto <70062747+mangto@users.noreply.github.com>
2022-07-22 03:13:27 +0900

    changing all system

--- cream_beta.py
+++ cream_beta.py
@@ -1,121 +1,77 @@
-import numpy
+import numpy, random
 
-import os
-os.system("cls")
+import Csys # -> for system control
 
+# Functions
 def sigmoid(value):
     return 1/(1+numpy.exp(-1*value))
 
-def RotateWeight(weights:numpy.array):
-    return numpy.flip(numpy.rot90(weights,k=-1),1)
+def ReLU(value, threshold=0):
+    return max(threshold, value)
 
-def MultiplyEach(a:numpy.array,b:numpy.array):
-    if (len(a) != len(b)): raise ValueError(f"Different inputs {len(a)}, {len(b)}")
-    return numpy.array([list(a[i]*b[i]) for i in range(len(a))])
+def Leaky_ReLU(value):
+    return max(0.01*value, value)
 
-def Multiply(a:numpy.array,b:numpy.array):
+def EReLU(value):
+    return max(0.01*sigmoid(value), value)
+
+#
+
+def CostFunction(output, target):
+    return (numpy.array(output)-numpy.array(target))**2/2
+
+def Multiply(a:numpy.array,b:numpy.array): # Multiply([[1, 2], [3, 4]], [1,2]) -> [[1, 4], [3, 8]]
     if (len(a[0]) != len(b)): raise ValueError(f"Different inputs ({len(a[0])}*{len(a)}), {len(b)}")
     return numpy.array([list(a[i]*b) for i in range(len(a))])
 
-def CostFunctionDerivative(output:list, real:list):
-    out = numpy.array(output)
-    real = numpy.array(real)
 
-    cost = 2*(out-real)
+class Network:
+    learning_rate = 0.3
 
-    return cost
+    def init_neurons(NetworkShape:list):
+        net =[[numpy.random.randn(NetworkShape[i]) for j in range(count)] for i, count in enumerate(NetworkShape[1:])]
+        return net
 
-def CostFunction(output:list, real:list):
-    out = numpy.array(output)
-    real = numpy.array(real)
+    def init_bias(NetworkShape:list):
+        biases = [numpy.random.randn(1)[0] for l in NetworkShape]
+        return biases 
 
-    cost = (out-real)**2
-
-    return cost
-
-def NeuronCounts(shape:list):
-    out = 0
-    for i in range(len(shape)-1):
-        out += shape[i]*shape[i+1]
-
-    return out
-
-class network:
-    def __init__(self, LearningRate:float, NetworkShape:list):
+    def __init__(self, NetworkShape:list, learning_rate:float=None, function=ReLU):
+        if (learning_rate != None): Network.learning_rate = learning_rate
+        
         self.NetworkShape = NetworkShape
-        self.weights = [RotateWeight(numpy.array([list(numpy.random.randn(i)) for j in range(NetworkShape[k+1])])) for k, i in enumerate(NetworkShape[0:-1])]
-        self.activations = self.reset_activation()
+        self.function = function
+        self.weights = Network.init_neurons(NetworkShape)
+        self.biases = Network.init_bias(NetworkShape)
 
-        self.l_rate = LearningRate
-        self.NueronCount = NeuronCounts(NetworkShape)
+        self.activations = self.reset_activation()
+        self.costs = numpy.zeros(NetworkShape[-1])
 
     def reset_activation(self):
-        self.activations = [ [0 for i in range(hn)] for hn in self.NetworkShape]
+        return [[0 for i in range(hn)] for hn in self.NetworkShape]     
 
-    def forward(self,inputs:list):
+    def train(self, PackCount:int, dataset:list, MaxEpoch:int=None, EndCost:float=0.01):
+        cost = 0
+        epoch = 0
+
+        while (cost > EndCost or epoch == 0 or (MaxEpoch != None and epoch <= MaxEpoch)):
+            epoch += 1
+            costs = numpy.zeros(self.NetworkShape[-1])
+
+            for i in range(PackCount):
+                data = random.sample(dataset, 1)[0]
+                self.forward(data[0])
+                costs += CostFunction(self.activations[-1], data[1])
+            costs /= PackCount
+
+    def forward(self, inputs:list):
         if (len(inputs) != self.NetworkShape[0]): raise ValueError("Wrong input counts")
-        self.reset_activation()
+        self.activations = self.reset_activation()
 
         self.activations[0] = inputs
-        for i in range(len(self.activations[0:-1])):
-            act = self.activations[i]
-            weights = self.weights[i]
-
-            active = sigmoid((numpy.sum(RotateWeight(weights)*act,axis=1)))
-
-            self.activations[i+1] = active
-
-    def PartialDerivative(self,weights:list, costs, SelfActivation):
-        result = 1 #assume cost is 1 -> multiply later
-            
-        if (len(weights) > 1):               
-            result = weights[0]
-            reverse = list(reversed(weights[1:-1]))
-            last = RotateWeight(weights[-1])
-            out = []
-
-            for i in range(self.NetworkShape[-1]):
-                c = last[i]
-                    
-
-                for weight in reverse:
-                    c = numpy.sum(Multiply(weight, c),axis=1)
-
-                out.append(c)
-            result = numpy.sum([numpy.sum(result*o,axis=0) for o in out]*costs)
-        output = SelfActivation*result
-        return output
-
-    def backpropgation(self, RValue:list):
-        if (len(RValue) != self.NetworkShape[-1]): raise ValueError("Worng Counts of 'RValue' (=RValue)")
-
-        cost = CostFunctionDerivative(self.activations[-1],RValue)
-
-        for l, layer in enumerate(self.weights):
-            for n, neuron in enumerate(layer):
-                for w, weight in enumerate(neuron):
-                    active = self.activations[l][n]
-
-                    
-                    weights = [self.weights[l+1][w]] + list(self.weights[l+2:])
-                    dw = self.PartialDerivative(weights, cost, active)
-                    self.weights[l][n][w] = self.weights[l][n][w] - self.weights[l][n][w]*dw*self.l_rate
-
-import random
-
-count = 100
-
-net = network(0.3,[3,4,4,1])
-
-net.forward([1,1,1])
-print(numpy.sum(CostFunction(net.activations[-1],[0.3])))
-print(net.weights)
-
-for i in range(count):
-    a = numpy.array([random.random() for i in range(3)])
-    net.forward(a)
-    net.backpropgation([numpy.sum(a*2)])
-
-net.forward([1,1,1])
-print(numpy.sum(CostFunction(net.activations[-1],[0.3])))
-print(net.weights)
+        
+        for l, neurons in enumerate(self.weights):
+            activation = self.activations[l]
+            new_activation = numpy.sum(Multiply(neurons, activation), axis=0)+self.biases[l]
+            new_activation = [self.function(a) for a in new_activation]
+            self.activations[l+1] = new_activation
