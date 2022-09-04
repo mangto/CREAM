d71964baf39b36305c0271a5379788fd2ca8ee95
mangomangto <70062747+mangto@users.noreply.github.com>
2022-09-04 20:57:46 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import cream_beta as cream
-import tool.Csys as Csys
-import tool.progress_bar as pb
-import numpy, random, time
-from threading import Thread
-
-Csys.clear()
-
-network = cream.network([1,2,2,1], ActivationFunction=cream.Leaky_ReLU,LearningRate=0.3)
