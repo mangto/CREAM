1564cccab5fca654b793801d711441d1b9400aa3
mangomangto <70062747+mangto@users.noreply.github.com>
2022-11-28 17:16:14 +0900

    Delete cream/visualizer directory

--- cream/visualizer/__init__.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from threading import Thread
-
-def run(argv:list = []):
-    thread = Thread(target=starter)
-    thread.start()
-
-def starter(argv:list = []):
-    subprocess.call(["python", 'visualizer\\network_visualizer.py'], shell=False)
