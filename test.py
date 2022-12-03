41c96190a16f2c137a9324f2efed40b9f7533f25
mangomangto <70062747+mangto@users.noreply.github.com>
2022-12-03 19:17:37 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-import cream, numpy
+import cream, random, numpy
 
+dataset = cream.datasets.XOR
 network = cream.network()
-network.add(cream.layer.Dense(20, cream.functions.sigmoid, InputShape=1000))
-# network.add(cream.layer.Dropout(0.2))
-network.add(cream.layer.Dense(10, cream.functions.sigmoid))
+network.add(cream.layer.Dense(5, cream.functions.ReLU, InputShape=2))
+network.add(cream.layer.Dense(1, cream.functions.ReLU))
 network.compile()
 
-print(network.foreward(numpy.random.randn(1000)))
+print(dataset)
+error = 1
+epoch = 0
+while (error > 0.1**15 and epoch <= 10000):
+    error = 0
+    for data in dataset:
+        network.forward(data[0])
+        network.backward(data[1])
+        error += sum(cream.functions.Error(network.activ[-1], data[1]))
+
+    cream.csys.out(f"epoch: {epoch:>6} | error: {error}", cream.csys.OKCYAN)
+    epoch += 1
+    
+    if (numpy.isnan(error)):
+        cream.csys.stop()
+
+for layer in network.layers:
+    cream.csys.out(layer.weights, cream.csys.OKBLUE)
+    cream.csys.out(layer.biases, cream.csys.OKGREEN)
+cream.csys.stop("yo..")
