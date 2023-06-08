d41eed0585ecd16c1b5dc0ad2e54e0d3a76e13b1
mangomangto <70062747+mangto@users.noreply.github.com>
2023-06-08 18:20:07 +0900

    Delete functions directory

--- functions/__init__.py
+++
@@ -1,2 +0,0 @@
-from cream.functions.activation import *
-from cream.functions.function import *
--- functions/function.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-def Error(output, target, dervative:bool=False):
-    if (dervative == False): return (numpy.array(output)-numpy.array(target))**2/2
-    else: return numpy.array(output)-numpy.array(target)
-
-def CosineSimilarity(a, b):
-    return (a@b)/(numpy.linalg.norm(a)*numpy.linalg.norm(b))
